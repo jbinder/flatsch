70d704a3c13c69d0f9d4defe1025cc53da1f9151
Johannes Binder <j.binder.x@gmail.com>
2018-05-10 12:26:41 +0200

    Initial commit, basic blink enforcement :)

---
+++ Flatsch/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Flatsch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const int ShowWindowTime = 200;
+        private const int HideWindowTime = 3000;
+
+        private readonly Timer _timer = new Timer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetWindowPosAndSize();
+        }
+
+        private void SetWindowPosAndSize()
+        {
+            Width = SystemParameters.WorkArea.Width;
+            Height = SystemParameters.WorkArea.Height;
+            Top = 0f;
+            Left = 0f;
+        }
+
+        /// <summary>
+        /// Try forcing the window to be always on top.
+        /// </summary>
+        private void MainWindow_OnDeactivated(object sender, EventArgs e)
+        {
+            var window = (Window)sender;
+            window.Topmost = true;
+        }
+
+        /// <summary>
+        /// Start the timer to show the message in a specified interval.
+        /// </summary>
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            SetHideWindowTimer();
+            _timer.Enabled = true;
+        }
+
+        private void SetHideWindowTimer()
+        {
+            _timer.Elapsed -= OnShowWindow;
+            _timer.Elapsed += OnHideWindow;
+            _timer.Interval = ShowWindowTime;
+        }
+
+        private void SetShowWindowTimer()
+        {
+            _timer.Elapsed -= OnHideWindow;
+            _timer.Elapsed += OnShowWindow;
+            _timer.Interval = HideWindowTime;
+        }
+
+        private void OnHideWindow(object sender, ElapsedEventArgs e)
+        {
+            Dispatcher.Invoke(() => { Visibility = Visibility.Collapsed; });
+            SetShowWindowTimer();
+        }
+
+        private void OnShowWindow(object sender, ElapsedEventArgs e)
+        {
+            Dispatcher.Invoke(() => { Visibility = Visibility.Visible; });
+            SetHideWindowTimer();
+        }
+    }
+}
